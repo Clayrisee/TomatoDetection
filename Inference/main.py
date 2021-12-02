68989ef8b3df79220d2db76d89b04af545f2e6e4
Clayrisee <ardikatamah@gmail.com>
2021-12-02 22:33:56 +0700

    update main.py

--- Inference/main.py
+++ Inference/main.py
@@ -3,7 +3,7 @@ import cv2
 
 model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx")
 
-img = cv2.imread("test_image/tomato37_png.rf.37c3c302d77c36ad7eb626901ec008cf.jpg")
+img = cv2.imread("test_image/test_image.jpg")
 
 # Test Predict
 model.predict(img)
