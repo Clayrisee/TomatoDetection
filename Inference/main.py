2629a3cd485c68af79f21a05849ab9682eb63ff7
Clayrisee <ardikatamah@gmail.com>
2021-12-02 21:45:22 +0700

    Add inference library folder

--- Inference/main.py
+++ Inference/main.py
@@ -5,4 +5,5 @@ model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx")
 
 img = cv2.imread("test_image/tomato37_png.rf.37c3c302d77c36ad7eb626901ec008cf.jpg")
 
+# Test Predict
 model.predict(img)
