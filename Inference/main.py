b05ea9b09f91d386e0a87aac2b79cf7a7d6f2893
Clayrisee <ardikatamah@gmail.com>
2021-12-03 08:06:34 +0700

    try formula from paper

--- Inference/main.py
+++ Inference/main.py
@@ -1,9 +1,17 @@
 from modul.tomato_detection.TomatoModel import TomatoModel
 import cv2
-
+import numpy as np
 model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx")
 
-img = cv2.imread("test_image/test_image.jpg")
+img = cv2.imread("test_image/test_image.jpg", 1)
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+red = rgb[:, :, 0]
+green = rgb[:, :, 1]
+blue = rgb[:, :, 2]
 
+print(red)
+sum_red = np.sum(red)
+print(sum_red)
+print(sum_red/ 255)
 # Test Predict
-model.predict(img)
+# model.predict(img)
