292e9308bca182f6f6af774f492982b05bce339e
manfredmichael <koskosanrider@gmail.com>
2021-12-03 14:05:17 +0700

    apply calibration to input images

--- Inference/main.py
+++ Inference/main.py
@@ -8,16 +8,15 @@ from modul.stereo_vision import calibration
 model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx")
 
 img = cv2.imread("test_image/test_image_2.jpg", 1)
-# imgL = cv2.imread("test_image/sample_left.jpeg", 1)
-# imgR = cv2.imread("test_image/sample_right.jpeg", 1)
-
+imgL = cv2.imread("test_image/sample_left.jpeg", 1)
+imgR = cv2.imread("test_image/sample_right.jpeg", 1)
 ## Calibration
-# imgL = cv2.resize(imgL, (640,480), interpolation=cv2.INTER_LINEAR)
-# imgR = cv2.resize(imgR, (640,480), interpolation=cv2.INTER_LINEAR)
-# print(imgL.shape)
-# print(imgR.shape)
+imgL = cv2.resize(imgL, (640,480), interpolation=cv2.INTER_LINEAR)
+imgR = cv2.resize(imgR, (640,480), interpolation=cv2.INTER_LINEAR)
+print(imgL.shape)
+print(imgR.shape)
 
-# imgL, imgR = calibration.undistortRectify(imgL, imgR)
+imgL, imgR = calibration.undistortRectify(imgL, imgR)
 
 # hsi_value = find_hsi_value(img)
 # print(hsi_value)
@@ -33,5 +32,5 @@ img = cv2.imread("test_image/test_image_2.jpg", 1)
 
 
 model.predict(img)
-# left_result_coors, left_labels = model.predict(imgL)
-# right_result_coors, right_labels = model.predict(imgR)
+left_result_coors, left_labels = model.predict(imgL)
+right_result_coors, right_labels = model.predict(imgR)
