997c2a2edad79b97d29b60d9875c1360a0040c30
manfredmichael <koskosanrider@gmail.com>
2021-12-05 08:47:00 +0700

    add more stereo image sample

--- Inference/main.py
+++ Inference/main.py
@@ -7,9 +7,9 @@ from modul.stereo_vision import calibration
 
 model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx")
 
-img = cv2.imread("test_image/test_image_2.jpg", 1)
-imgL = cv2.imread("test_image/sample_left.jpeg", 1)
-imgR = cv2.imread("test_image/sample_right.jpeg", 1)
+# img = cv2.imread("test_image/test_image_2.jpg", 1)
+imgL = cv2.imread("test_image/sample_left2.jpeg", 1)
+imgR = cv2.imread("test_image/sample_right2.jpeg", 1)
 ## Calibration
 imgL = cv2.resize(imgL, (640,480), interpolation=cv2.INTER_LINEAR)
 imgR = cv2.resize(imgR, (640,480), interpolation=cv2.INTER_LINEAR)
@@ -19,9 +19,10 @@ print(imgR.shape)
 imgL, imgR = calibration.undistortRectify(imgL, imgR)
 
 
-print(model.predict(img))
-# left_results = model.predict(imgL)
-# right_results = model.predict(imgR)
+# print(model.predict(img))
+left_results = model.predict(imgL)
+right_results = model.predict(imgR)
 
-# print(left_results)
-# print(right_results)
+import pprint
+pprint.pprint(left_results)
+pprint.pprint(right_results)
