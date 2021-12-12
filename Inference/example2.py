cc2fe942a3f4d01f4f6b6e57eaf9ca63b161e30d
Clayrisee <ardikatamah@gmail.com>
2021-12-12 15:04:04 +0700

    Add example of input streo vision

--- Inference/example2.py
+++ Inference/example2.py
@@ -2,12 +2,10 @@ from modul.stereo_vision import calibration
 from modul.tomato_detection.TomatoModel import TomatoModel
 import cv2
 
-model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx") # Load onnx model
+model = TomatoModel(onnx_path="model/tomato_detection_yolov4_tiny.onnx") # Load onnx model
 img_left = cv2.imread("test_image/sample_left.jpeg", 1) # Input image
 img_right = cv2.imread("test_image/sample_right.jpeg", 1)
 img_left, img_right = calibration.undistortRectify(img_left, img_right)
-results_left = model.predict(img_left)
-result_right = model.predict(img_right)
+final_result = model.predict_stereo_vision(img_left, img_right)
 
-print(results_left)
-print(result_right)
+print(final_result)
