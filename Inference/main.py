c06510feaed3f40070851a662202e5902cc01638
manfredmichael <koskosanrider@gmail.com>
2021-12-16 22:20:00 +0700

    Add get_pair method

--- Inference/main.py
+++ Inference/main.py
@@ -8,8 +8,8 @@ from utils import draw_predictions
 
 model = TomatoModel(onnx_path="model/yolov4_1_3_416_416_static.onnx")
 
-imgL = cv2.imread("test_image/sample_left.jpeg", 1)
-imgR = cv2.imread("test_image/sample_right.jpeg", 1)
+imgL = cv2.imread("test_image/sample_left2.jpeg", 1)
+imgR = cv2.imread("test_image/sample_right2.jpeg", 1)
 
 ## Calibration
 imgL = cv2.resize(imgL, (640,480), interpolation=cv2.INTER_LINEAR)
@@ -22,6 +22,9 @@ imgR_pred = model.predict(imgR_calibrated)
 pprint.pprint(imgL_pred)
 pprint.pprint(imgR_pred)
 
+imgL_pred_paired, imgR_pred_paired = object_pairing.get_pair(imgL_pred, imgR_pred)
+
  # PREDICTION DEBUG
-draw_predictions(imgL, imgL_calibrated, imgL_pred,
-                 imgR, imgR_calibrated, imgR_pred)
+pprint.pprint(imgL_pred_paired)
+draw_predictions(imgL, imgL_calibrated, imgL_pred_paired,
+                 imgR, imgR_calibrated, imgR_pred_paired)
